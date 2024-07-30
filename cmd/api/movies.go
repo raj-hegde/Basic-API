457fc9ad04f150c53bb5fd76bfc725c6c56fe347
raj-hegde <rajeevhegde11@gmail.com>
2024-07-30 15:51:54 +0530

    GetAll method added

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -181,6 +181,23 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 	input.Filters.Page = app.readInt(qs, "page", 1, v)
 	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
 	input.Filters.Sort = app.readString(qs, "sort", "id")
+	input.Filters.SortSafelist = []string{"id", "title", "year", "runtime", "-id", "-title", "-year", "-runtime"}
+
+	if data.ValidateFilters(v, input.Filters); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	movies, err := app.models.Movies.GetAll(input.Title, input.Genres, input.Filters)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movies}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 
 	if !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
